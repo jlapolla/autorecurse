f37236882cffba758a6accea7770c06fae20fc22
Justin LaPolla <j.a.lapolla@gmail.com>
2017-02-22 08:23:16 -0600

    FileSectionFilter class

--- python3/lib/test_generics.py
+++ python3/lib/test_generics.py
@@ -157,5 +157,21 @@ class TestEmptyLineFilter(unittest.TestCase):
         self.assertIs(obj.condition, True)
 
 
+class TestFileSectionFilter(unittest.TestCase):
+
+    def test_content(self):
+        obj = FileSectionFilter.make()
+        obj.current_item = Line.make('Hello')
+        self.assertIs(obj.condition, False)
+        obj.current_item = Line.make('# Files')
+        self.assertIs(obj.condition, False)
+        obj.current_item = Line.make('Hello')
+        self.assertIs(obj.condition, True)
+        obj.current_item = Line.make('Goodbye')
+        self.assertIs(obj.condition, True)
+        obj.current_item = Line.make('# files hash-table stats:')
+        self.assertIs(obj.condition, False)
+        obj.current_item = Line.make('Hello')
+        self.assertIs(obj.condition, False)
 
 
