c6c953c1e73927bdd3362a0471f59df4a946cec0
Justin LaPolla <j.a.lapolla@gmail.com>
2017-03-01 14:58:19 -0600

    Work on IteratorToInputStreamAdapter

--- python3/app/antlr/adapter.py
+++ python3/app/antlr/adapter.py
@@ -9,42 +9,83 @@ class IteratorToInputStreamAdapter(Iterator[str], InputStream):
 
     ### States
 
-    - I = Intermediate, Not empty
-    - E = End, Not empty
-    - EE = End, Empty
+    - I = Intermediate, Non-empty buffer
+    - E = End, Not-empty buffer
+    - EE = End, Empty buffer
 
     ### Transition Labels
 
     - Consume = Client calls consume
+    - Next = Client calls move_to_next
+    - End = Client calls move_to_end
 
     ### Transitions Grouped by Label
 
     - Consume
       - I -> I
       - I -> E
+    - Next
+      - I -> I
+      - I -> E
+    - End
+      - I -> E
+      - E -> E
+      - EE -> EE
+
+    ## Call State Validity
+
+    For each method listed, client is allowed to call the method in the
+    given states.
+
+    - consume: I
+    - current_item (getter): I
+    - has_current_item (getter): I E EE
+    - is_at_start (getter): I E EE
+    - is_at_end (getter): I E EE
+    - move_to_next: I
+    - move_to_end: I E EE
 
     ## Call Argument Validity
 
     For each method listed, client is allowed to call the method with
     the given parameters.
 
+    - LA(self, offset: int)
+      - offset is in self._buffer
     - seek(self, _index: int)
       - _index >= 0
-      - _index < self.index -> _index is still in self._buffer
+      - _index < self.index -> _index is in self._buffer
+
+    ## Call Results
+
+    For each state listed, calling the specified method will return the
+    given result.
+
+    - I
+      - has_current_item (getter): True
+      - is_at_start (getter): False
+      - is_at_end (getter): False
+    - E
+      - has_current_item (getter): False
+      - is_at_start (getter): False
+      - is_at_end (getter): True
+    - EE
+      - has_current_item (getter): False
+      - is_at_start (getter): False
+      - is_at_end (getter): True
 
     Notes:
 
-    - We are never at start state, because the semantics of InputStream
-      only has intermediate state and end state.
+    - We are never at start state. InputStream only has intermediate
+      state and end state.
     - In intermediate state, self.index < self.size.
     - In end state, self.index == self.size.
-    - self._iterator.has_current_item -> self._buffer.has_current_item
     """
 
     @staticmethod
     def _setup(instance: 'IteratorToInputStreamAdapter', iterator: Iterator[str]):
-        instance._global_index_wrapper = FifoGlobalIndxWrapper.make(LinkedFifo.make())
-        instance._buffer = FifoToManagedFifoAdapter.make(instance._global_index_wrapper)
+        instance._buffer_global = FifoGlobalIndexWrapper.make(LinkedFifo.make())
+        instance._buffer = FifoToManagedFifoAdapter.make(instance._buffer_global)
         instance._iterator = iterator
         IteratorToInputStreamAdapter._initialize_buffer(instance)
 
@@ -94,11 +135,20 @@ class IteratorToInputStreamAdapter(Iterator[str], InputStream):
                 # I -> E
                 self._buffer.move_to_next()
 
+    def move_to_end(self) -> None:
+        if self._is_I: # State I
+        elif self._is_E: # State E
+            # Already at end
+            pass
+        else: # State EE
+            # Already at end
+            pass
+
     @property
     def index(self) -> int:
         result = None
         if self.has_current_item: # State I
-            result = self._global_index_wrapper.current_global_index
+            result = self._buffer_global.current_global_index
         else: # State E or EE
             result = self.size
         return result
@@ -106,7 +156,7 @@ class IteratorToInputStreamAdapter(Iterator[str], InputStream):
     @property
     def size(self) -> int:
         # State I, E, or EE
-        return self._global_index_wrapper.global_count
+        return self._buffer_global.global_count
 
     def reset(self) -> None:
         raise NotImplementedError()
@@ -166,21 +216,21 @@ class IteratorToInputStreamAdapter(Iterator[str], InputStream):
         if self.has_current_item: # State I
             if _index >= self.index:
                 if _index >= self.size:
-                    self._global_index_wrapper.move_to_global_index(self.size - 1)
+                    self._buffer_global.move_to_global_index(self.size - 1)
                     diff = _index - self.index
                     while not (diff == 0 or self.is_at_end):
                         self.move_to_next()
                         diff = diff - 1
                 else:
-                    self._global_index_wrapper.move_to_global_index(_index)
+                    self._buffer_global.move_to_global_index(_index)
             else:
-                self._global_index_wrapper.move_to_global_index(_index)
+                self._buffer_global.move_to_global_index(_index)
         elif not self._buffer.is_empty: # State E
             if _index >= self.index:
                 # Already at end
                 pass
             else:
-                self._global_index_wrapper.move_to_global_index(_index)
+                self._buffer_global.move_to_global_index(_index)
         else: # State EE
             if _index >= self.index:
                 # Already at end
@@ -216,5 +266,14 @@ class IteratorToInputStreamAdapter(Iterator[str], InputStream):
             buffer_.write(self.current_item)
             self.move_to_next()
             count = count - 1
+
+    def _is_I(self) -> bool:
+        return self.has_current_item
+
+    def _is_E(self) -> bool:
+        return self.is_at_end and (not self._buffer.is_empty)
+
+    def _is_EE(self) -> bool:
+        return self.is_at_end and self._buffer.is_empty
 
 
