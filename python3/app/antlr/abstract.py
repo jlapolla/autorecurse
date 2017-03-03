5d8e20a259121bf5c1ab203d8978a21889d01600
Justin LaPolla <j.a.lapolla@gmail.com>
2017-03-03 14:29:03 -0600

    IteratorToTokenStreamAdapter class

--- python3/app/antlr/abstract.py
+++ python3/app/antlr/abstract.py
@@ -1,5 +1,7 @@
+from antlr4 import Token
 from abc import ABCMeta, abstractmethod
-from typing import TypeVar, Generic
+from io import StringIO
+from typing import TypeVar, Generic, Tuple
 import typing
 
 
@@ -55,6 +57,34 @@ class CharStream(IntStream):
 
     @abstractmethod
     def getText(self, start: int, stop: int) -> str:
+        pass
+
+
+class TokenStream(IntStream):
+
+    @abstractmethod
+    def get(self, index: int) -> Token:
+        pass
+
+    def getText(self, interval: Tuple[int, int] = None) -> str:
+        if interval is not None:
+            index = interval[0]
+            stop_index = interval[1] + 1
+            with StringIO() as strbuffer:
+                while index != stop_index:
+                    strbuffer.write(self.get(index).text)
+                    index = index + 1
+                return strbuffer.getvalue()
+        else:
+            return self.getText((0, self._total_stream_size))
+
+    @abstractmethod
+    def LT(self, offset: int) -> Token:
+        pass
+
+    @property
+    @abstractmethod
+    def _total_stream_size(self) -> int:
         pass
 
 
