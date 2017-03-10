639fec3142fda54290ea25f648a04fffea99039a
Justin LaPolla <j.a.lapolla@gmail.com>
2017-03-10 06:55:05 -0600

    Preserve streaming parse pipeline

--- python3/app/lib/gnumake.py
+++ python3/app/lib/gnumake.py
@@ -39,4 +39,22 @@ class GnuMake:
         makefile_target_iterator = MakefileRuleParserToIteratorAdapter.make(makefile_rule_parser)
         return makefile_target_iterator
 
+    @staticmethod
+    def make_target_iterator_for_file_streaming(fp: io.TextIOBase) -> Iterator[MakefileTarget]:
+        file_lines = FileLineIterator.make(fp)
+        file_section = ConditionalSkipIterator.make(file_lines, FileSectionFilter.make())
+        file_section_no_comments = ConditionalSkipIterator.make(file_section, InformationalCommentFilter.make())
+        file_section_chars = LineToCharIterator.make(file_section_no_comments)
+        char_stream_1 = IteratorToCharStreamAdapter.make(file_section_chars)
+        paragraph_lexer = TargetParagraphLexer(char_stream_1)
+        paragraph_tokens = TokenSourceToIteratorAdapter.make(paragraph_lexer)
+        paragraph_chars = TokenToCharIterator.make(paragraph_tokens)
+        char_stream_2 = IteratorToCharStreamAdapter.make(paragraph_chars)
+        makefile_rule_lexer = MakefileRuleLexer(char_stream_2)
+        makefile_rule_tokens = TokenSourceToIteratorAdapter.make(makefile_rule_lexer)
+        token_stream_1 = IteratorToTokenStreamAdapter.make(makefile_rule_tokens)
+        makefile_rule_parser = MakefileRuleParser(token_stream_1)
+        makefile_target_iterator = MakefileRuleParserToIteratorAdapter.make(makefile_rule_parser)
+        return makefile_target_iterator
+
 
