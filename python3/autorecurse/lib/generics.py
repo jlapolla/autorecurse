5c2451ff31d3edf4fe9f18b30ec3d3e7d3a936b3
Justin LaPolla <j.a.lapolla@gmail.com>
2017-03-13 13:01:17 -0500

    Extract Condition class into stream module

--- python3/autorecurse/lib/generics.py
+++ python3/autorecurse/lib/generics.py
@@ -5,6 +5,7 @@ import io
 from typing import TypeVar, Generic
 import typing
 from autorecurse.lib.line import LineBreakError, Line
+from autorecurse.lib.stream import Condition
 
 
 T_co = TypeVar('T_co', covariant=True)
@@ -340,67 +341,6 @@ class Buffer(Iterator[T_co]):
             self.move_to_index(index)
 
 del T_co
-
-
-T_contra = TypeVar('T_contra', contravariant=True)
-class Condition(Generic[T_contra], metaclass=ABCMeta):
-    """
-    Stateful stream evaluator.
-
-    ## Transition System Definition
-
-    ### States
-
-    - S = Start condition
-    - Y = Condition is True
-    - N = Condition is False
-
-    ### Transition Labels
-
-    - Recieve = Client calls current_item (setter)
-
-    ### Transitions Grouped by Label
-
-    - Recieve
-      - S -> Y
-      - S -> N
-      - Y -> Y
-      - Y -> N
-      - N -> Y
-      - N -> N
-
-    ## Call Validity
-
-    For each method listed, client is allowed to call the method in the
-    given states.
-
-    - current_item (setter): S Y N
-    - condition (getter): Y N
-
-    ## Call Results
-
-    For each state listed, calling the specified method will return the
-    given result.
-
-    - Y
-      - condition (getter): True
-    - N
-      - condition (getter): False
-    """
-
-    @abstractmethod
-    def _set_current_item(self, value: T_contra) -> None:
-        pass
-
-    current_item = property(None, _set_current_item)
-
-    @property
-    @abstractmethod
-    def condition(self) -> bool:
-        pass
-
-del Condition._set_current_item
-del T_contra
 
 
 T = TypeVar('T')
