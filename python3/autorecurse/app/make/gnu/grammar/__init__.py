11a57d9d738830b61b3bf5b5dcd7f1a2fe5131a0
Justin LaPolla <j.a.lapolla@gmail.com>
2017-03-13 12:16:53 -0500

    Move FileSection and InformationalComment filters

--- python3/autorecurse/app/make/gnu/grammar/__init__.py
+++ python3/autorecurse/app/make/gnu/grammar/__init__.py
@@ -1,4 +1,5 @@
 from autorecurse.app.make.gnu.grammar.MakefileRuleLexer import MakefileRuleLexer
 from autorecurse.app.make.gnu.grammar.MakefileRuleParser import MakefileRuleParser
 from autorecurse.app.make.gnu.grammar.TargetParagraphLexer import TargetParagraphLexer
+from autorecurse.app.make.gnu.grammar.filter import FileSectionFilter, InformationalCommentFilter
 
---
+++ python3/autorecurse/app/make/gnu/grammar/test_filter.py
@@ -0,0 +1,22 @@
+from autorecurse.app.make.gnu.grammar.filter import *
+import unittest
+
+
+class TestFileSectionFilter(unittest.TestCase):
+
+    def test_content(self):
+        obj = FileSectionFilter.make()
+        obj.current_item = Line.make('Hello')
+        self.assertIs(obj.condition, False)
+        obj.current_item = Line.make('# Files')
+        self.assertIs(obj.condition, False)
+        obj.current_item = Line.make('Hello')
+        self.assertIs(obj.condition, True)
+        obj.current_item = Line.make('Goodbye')
+        self.assertIs(obj.condition, True)
+        obj.current_item = Line.make('# files hash-table stats:')
+        self.assertIs(obj.condition, False)
+        obj.current_item = Line.make('Hello')
+        self.assertIs(obj.condition, False)
+
+
