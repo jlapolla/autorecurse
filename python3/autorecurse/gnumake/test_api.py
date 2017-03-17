4335cc91af9c131f5a2ddad607e5eb8a358c2e70
Justin LaPolla <j.a.lapolla@gmail.com>
2017-03-17 08:50:11 -0500

    Test GnuMake.nested_makefiles function directly

--- python3/autorecurse/gnumake/test_api.py
+++ python3/autorecurse/gnumake/test_api.py
@@ -5,6 +5,29 @@ import unittest
 
 class TestGnuMake(unittest.TestCase):
 
+    def test_nested_makefiles(self):
+        gnu = GnuMake.get_instance()
+        with gnu.nested_makefiles('test_sample/gnu/nested-makefiles') as it:
+            self.assertIs(it.is_at_start, True)
+            it.move_to_next()
+            makefile = it.current_item
+            self.assertEqual(makefile.exec_path, 'test_sample/gnu/nested-makefiles')
+            self.assertEqual(makefile.file_path, 'GNUmakefile')
+            it.move_to_next()
+            makefile = it.current_item
+            self.assertEqual(makefile.exec_path, 'test_sample/gnu/nested-makefiles/make-folder-2')
+            self.assertEqual(makefile.file_path, 'makefile')
+            it.move_to_next()
+            makefile = it.current_item
+            self.assertEqual(makefile.exec_path, 'test_sample/gnu/nested-makefiles/make-folder-1')
+            self.assertEqual(makefile.file_path, 'makefile')
+            it.move_to_next()
+            makefile = it.current_item
+            self.assertEqual(makefile.exec_path, 'test_sample/gnu/nested-makefiles/make-folder-1/subfolder')
+            self.assertEqual(makefile.file_path, 'Makefile')
+            it.move_to_next()
+            self.assertIs(it.is_at_end, True)
+
     def test_execution_directory(self):
         gnu = GnuMake.get_instance()
         self.assertIs(gnu.execution_directory('-h'.split()), None)
