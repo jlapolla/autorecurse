ed73aa36ded51414c6292b244a4fa32a97ce3025
Justin LaPolla <j.a.lapolla@gmail.com>
2017-04-18 14:14:07 -0500

    Basic setup.py

---
+++ python3/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name='autorecurse',
+    version='1.0.0a1',
+    description='Wraps the make build utility and automatically calls make recursively on makefiles in subdirectories.',
+    author='Justin LaPolla',
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
+    install_requires=['antlr4-python3-runtime ~= 4.5, < 4.6'],
+    zip_safe=False
+        )
+
+
